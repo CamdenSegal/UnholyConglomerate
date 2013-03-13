74d0cdbb9c9915f664fce09f55cc4e9738987bc4
Camden Segal <camden@camdensegal.com>
2013-03-13 00:16:35 -0400

    Got it working!
    Next up: implement player movement.

--- unholyconglomerate.js
+++ unholyconglomerate.js
@@ -10,8 +10,11 @@ var Game = {
 		var loadingText = document.getElementById("loading");
 		loadingText.parentNode.removeChild(loadingText);
 
+		this.player = null;
+		this.engine = new ROT.Engine();
+
 		this._generateMap();
-		this._drawWholeMap();
+		this._drawVisibleMap(40,12,10);
 	},
 
 	_generateMap: function() {
@@ -24,27 +27,94 @@ var Game = {
 			}
 
 			if(value) {
-				this.map[x][y] = Tiles.wall;
+				this.map[x][y] = new WallTile();
 			}else{
-				this.map[x][y] = Tiles.floor;
+				this.map[x][y] = new FloorTile();
 			}
 		}
 
 		digger.create(digCallback.bind(this));
+
+		this._createPlayer();
 	},
 
-	_drawWholeMap: function() {
-		for(var x in this.map){
-			for(var y in this.map[x]){
-				this.display.draw(x,y,this.map[x][y].character);
-			}
+	_createPlayer: function() {
+		this.player = new Player(40,12);
+		this.engine.addActor(this.player);
+	},
+
+	_drawVisibleMap: function(x,y,r){
+		var fov = new ROT.FOV.PreciseShadowcasting(this._lightPasses.bind(this));
+
+		var fovCallback = function(x,y,d,visibility){
+			this.map[x][y].draw(x,y,d/r);
 		}
+		fov.compute(x,y,r,fovCallback.bind(this));
+		this.player.draw();
+	},
+
+	_lightPasses : function(x,y){
+		return this.map[x][y].lightPasses;
 	}
 }
 
-
-
-var Tiles = {
-	floor: {character: '.',color: '#fff',description:'empty floor'},
-	wall: {character: '#',color: '#fff',description:'rock wall'}
+//Tile classes
+var Tile = function(params){
+	this.character = '*';
+	this.fg = [255,255,255];
+	this.bg = [0,0,0];
+	this.description = "a point in space";
+	this.lightPasses = true;
+	for(var param in params){
+		this[param] = params[param];
+	}
 }
+Tile.prototype.draw = function(x, y, brightness){
+	Game.display.draw(x,y,this.character,ROT.Color.toHex(ROT.Color.interpolate(this.fg,[0,0,0],brightness)),ROT.Color.toHex(ROT.Color.interpolate(this.bg,[0,0,0],brightness)));
+}
+
+var FloorTile = function(params){
+	Tile.call(this, params);
+	this.character = '.';
+	this.fg = ROT.Color.randomize([230,230,230],[20,20,10]);
+	this.description = "open floor";
+	for(var param in params){
+		this[param] = params[param];
+	}
+}
+FloorTile.prototype = new Tile();
+
+var WallTile = function(params){
+	Tile.call(this, params);
+	this.character = ' ';
+	this.fg = [0,0,0];
+	this.bg = ROT.Color.randomize([250,250,250],[5,5,5]);
+	this.description = "rock wall";
+	this.lightPasses = false;
+	for(var param in params){
+		this[param] = params[param];
+	}
+}
+WallTile.prototype = new Tile();
+
+//Actor class
+var Actor = function(x,y){
+	this._x = x;
+	this._y = y;
+	this._speed = 100;
+	this._character = 'M';
+	this._color = [255,100,100];
+}
+Actor.prototype.draw = function(){
+	Game.display.draw(this._x, this._y,this._character, ROT.Color.toHex(this._color));
+}
+Actor.prototype.getSpeed = function(){
+	return this._speed;
+}
+
+var Player = function(x,y) {
+	Actor.call(this,x,y);
+	this._character = '@';
+	this._color = [100,255,100];
+}
+Player.prototype = new Actor();
